c6e78889c56cb2f2f5e47f5edaf4a2645825812b
Alan Coomes <acoomes33@gmail.com>
2022-12-13 19:50:52 -0700

    finished inon input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,85 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--bottomBorderSize': 1 + 'px',
+    '--fontSize': 14/16 + 'rem',
+    '--height' : 1.5 + 'rem',
+    '--paddingLeft': 24 + 'px',
+  },
+  large: {
+    '--bottomBorderSize': 2 + 'px',
+    '--fontSize': 18/16 + 'rem',
+    '--height': (36/16) + 'rem',
+    '--paddingLeft': 36 + 'px',
+  }
+}
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const styles = SIZES[size]
+  const iconSize = size === 'small' ? 16 : 24  ;
+  const iconStrokeWidth = size === 'small' ? 1 : 2;
+  return (
+  <Wrapper>
+    <VisuallyHidden>{label}</VisuallyHidden>
+    <IconWrapper style={{'--size': iconSize + 'px'}}>
+      <Icon size={iconSize} strokeWidth={iconStrokeWidth} id={icon}></Icon>
+    </IconWrapper>
+    <TextInput id={label} {...delegated} width={width} style={styles}>
+    </TextInput>
+  </Wrapper>
+  );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black}
+  }
+`;
+
+const IconWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: auto 0;
+    color: inherit;
+    width: var(--size);
+    height: var(--size);
+`;
+
+const TextInput = styled.input`
+  height: var(--height);
+  font-size: var(--fontSize);
+  /* width: var(--width); */
+  width: ${p => p.width + 'px'};
+  border: none;
+  border-bottom: var(--bottomBorderSize) solid ${COLORS.black};
+  padding-left: var(--paddingLeft);
+  color: inherit;
+  font-weight: 700;
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+
+`;
+
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Columns } from 'react-feather';
 import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
@@ -9,10 +10,35 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <SelectWrapper>
+      Newest Releases
+    <SelectHide value={value} onChange={onChange}>
       {children}
-    </select>
+    </SelectHide>
+    <IconWrapper>
+    <Icon id='chevron-down'></Icon>
+    </IconWrapper>
+    </SelectWrapper>
   );
 };
+
+const SelectWrapper = styled.div`
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  width: fit-content;
+  text-align: center;
+`;
+
+const SelectHide = styled.select`
+opacity: 0;
+position: absolute;
+z-index: 1;
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  font-weight: 900;
+`;
 
 export default Select;
